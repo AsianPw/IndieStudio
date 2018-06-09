a6827f6091d9821a70682a588e9e3b21e0d316ee
Sadisadou <bastien.guillaumat@epitech.eu>
2018-06-09 17:13:19 +0200

    IPlayer_correction

--- inc/IPlayer.hpp
+++ inc/IPlayer.hpp
@@ -11,15 +11,11 @@
 #include <iostream>
 
 class IPlayer {
-	public:
+public:
 	virtual ~IPlayer() = default;
-<<<<<<< HEAD
 	virtual void	IPlayer() = 0;
-	virtual void	setPlayerOnMap(std::vector<std::vector<char>> &) = 0;
-=======
 	virtual void	setPlayeronMap(std::vector<std::vector<char>> &, int ) = 0;
 	virtual void	setIaonMap(std::vector<std::vector<char>> &, int nbrIa) = 0;
->>>>>>> 6eca8f60930c71c3669d441151bda2c71b123a06
 	virtual void	moveLeft(std::vector<std::vector<char>> &) = 0;
 	virtual void	moveRight(std::vector<std::vector<char>> &) = 0;
 	virtual void	moveUp(std::vector<std::vector<char>> &) = 0;
